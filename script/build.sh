34d43aa3fa98e02e699e2c9cb13ca3ab7b5cf576
Ian Bytchek <ianbytchek@gmail.com>
2015-06-08 17:45:40 +0400

    Debian alternative

--- script/build.sh
+++ script/build.sh
@@ -1,28 +1,41 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 cd $(dirname $0)
 
 # Install apk packages and remove apk cache when finished.
 
-apk --update add \
+cat <<-EOL > /etc/apt/sources.list.d/nginx.list
+	deb http://nginx.org/packages/debian/ jessie nginx
+	deb-src http://nginx.org/packages/debian/ jessie nginx
+EOL
+
+apt-get update
+apt-get install --no-install-recommends --yes -t sid \
+    imagemagick \
     nginx \
-    php-fpm \
+    php5-fpm \
+    php5-imagick \
+    php5-mcrypt \
+    php5-mongo \
     supervisor
 
-rm -rf /var/cache/apk/*
-
-rm -rf /var/www/*
+rm -rf \
+    /tmp/* \
+    /var/cache/* \
+    /var/lib/apt/lists/* \
+    /var/tmp/* \
+    /var/www/*
 
 # Update php configuration.
 
 sed -i \
-    -e 's/group =.*/group = nginx/' \
-    -e 's/user =.*/user = nginx/' \
-    -e 's/listen\.owner.*/listen\.owner = nginx/' \
-    -e 's/listen\.group.*/listen\.group = nginx/' \
-    -e 's/error_log =.*/error_log = \/dev\/stdout/' \
-    /etc/php/php-fpm.conf
+    -e 's/group =.*/group = www-data/' \
+    -e 's/user =.*/user = www-data/' \
+    -e 's/listen\.owner.*/listen.owner = www-data/' \
+    -e 's/listen\.group.*/listen.group = www-data/' \
+    -e 's/listen = .*$/listen = 127.0.0.1:9000/' \
+    /etc/php5/fpm/pool.d/www.conf
 
 sed -i \
     -e '/open_basedir =/s/^/\;/' \
-    /etc/php/php.ini
+    /etc/php5/fpm/php.ini
